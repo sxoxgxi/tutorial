309a41622a11555f560948d6462318751f153c52
sxoxgxi <nirmalrawal198@gmail.com>
2022-06-02 14:03:46 +0545

    constants

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 fn main() {
-    let x = 6;
-    println!("x is: {}", x);
+    const SECONDS_PER_MINUTE: u32 = 60;
+    println!("{}", SECONDS_PER_MINUTE);
 
-    {
-        let x = x - 2;
-        println!("x is: {}", x);
-    }
 
-    let x = x + 1;
-    println!("x is: {}", x);
+    // let x = 6;
+    // println!("x is: {}", x);
+
+    // {
+    //     let x = x - 2;
+    //     println!("x is: {}", x);
+    // }
+
+    // let x = x + 1;
+    // println!("x is: {}", x);
 
     // let mut y = 6;
     // println!("y is: {}", y);
