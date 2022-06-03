7de3a5c9c05bc6850171dcd3fb2cb0ca1ab1032a
sxoxgxi <nirmalrawal198@gmail.com>
2022-06-03 17:19:21 +0545

    datatypes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,36 @@
 fn main() {
-    const SECONDS_PER_MINUTE: u32 = 60;
-    println!("{}", SECONDS_PER_MINUTE);
+    // integers
+    let x: i32 = 56;
+    let y: u32 = 56;
 
+    // float
+    let floating_point: f32 = 10.92;
+    
+    // boolean 
+    let false_boolean: bool = false;
+    let true_boolean: bool = true;
+
+    // character
+    let letter: char = 'a';
+
+    // tuple
+    let tup: (i32, bool, char)= (1, true, 'a');
+    println!("first one: {}", tup.1);
+
+    let mut tup2: (i8, bool, char)= (1, true, 'a');
+    tup2.0 = 5;
+    println!("first one: {}", tup2.0);
+
+    // array
+    let mut arr = [1, 2, 3, 4, 5];
+    arr[4] = 3;
+    println!("{}", arr[4]);
+
+    let mut arr2: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("{}", arr2[3])
+
+    // const SECONDS_PER_MINUTE: u32 = 60;
+    // println!("{}", SECONDS_PER_MINUTE);
 
     // let x = 6;
     // println!("x is: {}", x);
