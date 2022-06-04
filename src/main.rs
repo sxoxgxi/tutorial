56b41960b65ffd2f5926b6599fbb78d3b2844a1a
sxoxgxi <nirmalrawal198@gmail.com>
2022-06-04 17:44:42 +0545

    user input

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,39 @@
+use std::io;
+
 fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("failed to read line");
+    println!("{}", input);
+
     // integers
-    let x: i32 = 56;
-    let y: u32 = 56;
+    // let x: i32 = 56;
+    // let y: u32 = 56;
 
     // float
-    let floating_point: f32 = 10.92;
+    // let floating_point: f32 = 10.92;
     
     // boolean 
-    let false_boolean: bool = false;
-    let true_boolean: bool = true;
+    // let false_boolean: bool = false;
+    // let true_boolean: bool = true;
 
     // character
-    let letter: char = 'a';
+    // let letter: char = 'a';
 
     // tuple
-    let tup: (i32, bool, char)= (1, true, 'a');
-    println!("first one: {}", tup.1);
+    // let tup: (i32, bool, char)= (1, true, 'a');
+    // println!("first one: {}", tup.1);
 
-    let mut tup2: (i8, bool, char)= (1, true, 'a');
-    tup2.0 = 5;
-    println!("first one: {}", tup2.0);
+    // let mut tup2: (i8, bool, char)= (1, true, 'a');
+    // tup2.0 = 5;
+    // println!("first one: {}", tup2.0);
 
     // array
-    let mut arr = [1, 2, 3, 4, 5];
-    arr[4] = 3;
-    println!("{}", arr[4]);
+    // let mut arr = [1, 2, 3, 4, 5];
+    // arr[4] = 3;
+    // println!("{}", arr[4]);
 
-    let mut arr2: [i32; 5] = [1, 2, 3, 4, 5];
-    println!("{}", arr2[3])
+    // let mut arr2: [i32; 5] = [1, 2, 3, 4, 5];
+    // println!("{}", arr2[3])
 
     // const SECONDS_PER_MINUTE: u32 = 60;
     // println!("{}", SECONDS_PER_MINUTE);
